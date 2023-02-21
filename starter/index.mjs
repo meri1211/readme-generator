0c401a019527cd86624e1836e670719b4dbc1c1b
meri <mariana_asavoaiei@outlook.com>
2023-02-21 23:34:19 +0000

    created the template for the readme and added the user input as variables for the content

--- starter/index.mjs
+++ starter/index.mjs
@@ -1,7 +1,6 @@
 //const fs = require("fs");
-import fs from 'fs/promises'
+import fs from "fs/promises";
 //const path = require("path");
-
 
 //const inquirer = require("inquirer");
 import inquirer from "inquirer";
@@ -32,7 +31,19 @@ function init() {
  function call to initialize program
 init();*/
 
-let response = await inquirer
+let {
+  title,
+  description,
+  contents,
+  installation,
+  usage,
+  license,
+  contributors,
+  testing,
+  github,
+  email,
+  choices,
+} = await inquirer
   .prompt([
     {
       type: "input",
@@ -63,7 +74,7 @@ let response = await inquirer
       type: "checkbox",
       message: "What license are you using?",
       name: "license",
-      choices: ["MIT", "Apache", "Mozilla", "IBM", "Perl", "Eclipse"],
+      choices: ["MIT", "Mozilla", "IBM"],
     },
     {
       type: "input",
@@ -80,7 +91,52 @@ let response = await inquirer
       message: "Any future questions?",
       name: "questions",
     },
+    {
+      type: "input",
+      message: "What is your GitHub url?",
+      name: "github",
+    },
+    {
+      type: "input",
+      message: "What is your e-mail address?",
+      name: "email",
+    },
   ])
   .then((response) => {
     console.log(response);
   });
+
+  let readmeContent = 
+  `# Title
+  ${title}
+
+  ## Description
+  ${description}
+
+  ## Table of contents
+  ${contents}
+
+  ## Installation
+  ${installation}
+
+  ## Usage
+  ${usage}
+
+  ## License
+  ${license}
+
+  ## Contributors
+  ${contributors}
+
+  ## Testing
+  ${testing}
+
+  ## Questions
+
+  ### "Please check my Github profile!" +
+  ${github}
+  ### "For further questions, please make sure to contact me at my e-mail address:" +
+  ${email}`
+
+  fs.writeFile("README.md", readmeContent)
+ 
